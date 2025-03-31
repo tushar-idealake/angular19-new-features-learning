817420095a2b70f7cfd020428461ecd9558e6b45
Connie Leung <cleung334@gmail.com>
2025-03-31 23:32:36 +0800

    feat: httpResource equality function

--- projects/http-resource-demo/src/app/app.component.ts
+++ projects/http-resource-demo/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { ChangeDetectionStrategy, Component, VERSION } from '@angular/core';
+import { httpResource } from '@angular/common/http';
+import { ChangeDetectionStrategy, Component, linkedSignal, signal, VERSION } from '@angular/core';
+import { Joke, JokeAudit, jokeSchema } from './schemas/joke.schema';
 
 @Component({
   selector: 'app-root',
@@ -13,4 +15,40 @@ export class AppComponent {
   ];
   name = 'httpResource equality function';
   description = 'httpResource to query data';
+
+  numClicked = signal(0);
+  category = signal('');
+
+  jokeResource = httpResource(() => 
+  this.category() ? 
+  `https://v2.jokeapi.dev/joke/${this.category()}?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single&idRange=1-5`: undefined, {
+    parse: (raw) => jokeSchema.parse(raw),
+    equal: (a, b) => {
+      if (!a) {
+        return !b;
+      }
+
+      return a.id == b.id;
+    },
+  });
+
+  jokeAudit = linkedSignal<{ joke: Joke | undefined }, JokeAudit | undefined>({
+    source: () => ({ joke: this.jokeResource.value() }),
+    computation: (source, previous) => {
+      if (!source.joke) {
+        return undefined;
+      }
+      const numUpdates = previous?.value?.numUpdates || 0;
+      return { 
+        ...source.joke,
+        numUpdates: numUpdates + 1,
+      };
+    }
+  });
+
+  generateJoke() {
+    this.category.set('Programming');
+    this.numClicked.update((prev) => prev + 1);
+    this.jokeResource.reload();
+  }
 }

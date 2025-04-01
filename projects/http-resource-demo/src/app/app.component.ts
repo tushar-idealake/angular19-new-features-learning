a68cdf55a34d874069ecabc531d5c2f8ab60cdb1
Connie Leung <cleung334@gmail.com>
2025-04-01 09:19:26 +0800

    refactor: does not return undefined

--- projects/http-resource-demo/src/app/app.component.ts
+++ projects/http-resource-demo/src/app/app.component.ts
@@ -1,6 +1,12 @@
 import { httpResource } from '@angular/common/http';
-import { ChangeDetectionStrategy, Component, linkedSignal, signal, VERSION } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, Injector, linkedSignal, signal, VERSION } from '@angular/core';
 import { Joke, JokeAudit, jokeSchema } from './schemas/joke.schema';
+
+const jokeEquality = (a: Joke, b: Joke) => {
+  const isEqual = a.id == b.id;
+  console.log('isEqual', a.id, b.id, isEqual);
+  return isEqual;
+}
 
 @Component({
   selector: 'app-root',
@@ -18,27 +24,27 @@ export class AppComponent {
 
   numClicked = signal(0);
   category = signal('');
+  injector = inject(Injector);
 
   jokeResource = httpResource(() => 
   this.category() ? 
   `https://v2.jokeapi.dev/joke/${this.category()}?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single&idRange=1-5`: undefined, {
     parse: (raw) => jokeSchema.parse(raw),
-    equal: (a, b) => {
-      if (!a) {
-        return !b;
-      }
-
-      return a.id == b.id;
+    equal: (a, b) => jokeEquality(a, b),
+    defaultValue: {
+      id: -1,
+      error: false,
+      joke: '',
+      category: '',
     },
+    injector: this.injector,
   });
 
-  jokeAudit = linkedSignal<{ joke: Joke | undefined }, JokeAudit | undefined>({
+  jokeAudit = linkedSignal<{ joke: Joke }, JokeAudit>({
     source: () => ({ joke: this.jokeResource.value() }),
     computation: (source, previous) => {
-      if (!source.joke) {
-        return undefined;
-      }
-      const numUpdates = previous?.value?.numUpdates || 0;
+      const previousUpdates = previous?.value?.numUpdates;
+      const numUpdates = typeof previousUpdates !== 'undefined' ? previousUpdates : -1;
       return { 
         ...source.joke,
         numUpdates: numUpdates + 1,

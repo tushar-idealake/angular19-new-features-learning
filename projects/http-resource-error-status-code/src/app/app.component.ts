c8ab11d0462e8fcc7748ea81f703738329348bcf
Connie Leung <cleung334@gmail.com>
2025-04-21 14:10:59 +0800

    refactor: update app component

--- projects/http-resource-error-status-code/src/app/app.component.ts
+++ projects/http-resource-error-status-code/src/app/app.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChangeDetectionStrategy, Component, VERSION } from '@angular/core';
+import { HttpResourcePokemonComponent } from './http/httpresource-pokemon.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [HttpResourcePokemonComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'httpResourceErrorStatusCode';
+  version = VERSION.full;
+  prs = [
+    'https://github.com/angular/angular/pull/60802',
+  ];
+  name = 'httpResource error status code and header Demo';
+  description = 'Setting response header and status code during error';
 }
